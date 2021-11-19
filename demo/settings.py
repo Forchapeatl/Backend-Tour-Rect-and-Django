d1a99b73344a55cd94b016caffd6fdf6e661443a
FORCHA <forchapearl1@gmail.com>
2021-11-19 19:15:47 +0100

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'gsrcduhu9_l+9)*qdr01^%b8ue4@idq91nqd3xwh0lmz7p*bg+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://lamina.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
