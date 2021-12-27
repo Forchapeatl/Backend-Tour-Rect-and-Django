d1ef276770e037b24da8d74f535f1f7afe77eeec
FORCHA <forchapearl1@gmail.com>
2021-12-27 13:18:52 +0100

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-cors-middleware'
+    'django-cors-middleware',
     'oauth2_provider',
     'rest_framework',
     'django_filters',
